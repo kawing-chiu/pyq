865024f3333425a5a6bb1cfc9c9207be350b9ddb
Zhao Jiarong <kawing.chiu.sysu@gmail.com>
2015-09-09 16:25:07 +0800

    Add basic setup.py.

--- setup.py
+++ setup.py
@@ -5,4 +5,21 @@ import platform
 from setuptools import setup
 
 
-setup()
+version = '0.0.0'
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='pyq',
+    version=version,
+    author='Zhao Jiarong',
+    author_email='kawing.chiu.sysu@gmail.com',
+    url='',
+    description="",
+    long_description=readme,
+    license='GNU Affero GPL v3.0',
+    packages=['pyq'],
+    install_requires=[],
+    classifiers=[],
+)
